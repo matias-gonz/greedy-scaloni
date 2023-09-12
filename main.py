549d8e35beedba9cfcbfd241ff469d37c1294780
matias-gonz <maigonzalez@fi.uba.ar>
2023-09-12 15:36:53 -0300

    Agregar algoritmos

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+from typing import List, Tuple
+
+
+def greedy_scaloni_por_ayudante(tiempos: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
+    """
+    :param tiempos: lista de tuplas con los tiempos de Scaloni y ayudante (S_i, A_i)
+    :return: lista de ordenada por A_i en orden descendiente
+    """
+    return sorted(tiempos, key=lambda t: t[1], reverse=True)
+
+
+def greedy_scaloni_por_diferencia(tiempos: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
+    """
+    :param tiempos: lista de tuplas con los tiempos de Scaloni y ayudante (S_i, A_i)
+    :return: lista de ordenada por A_i - S_i en orden descendiente
+    """
+    return sorted(tiempos, key=lambda t: t[1] - t[0], reverse=True)
+
+
+def greedy_scaloni_por_scaloni(tiempos: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
+    """
+    :param tiempos: lista de tuplas con los tiempos de Scaloni y ayudante (S_i, A_i)
+    :return: lista de ordenada por S_i en orden ascendente
+    """
+    return sorted(tiempos, key=lambda t: t[0])
+
+
 def main():
-    print("Hello World!")
+    tiempos = [(1, 1), (3, 2), (2, 3), (4, 4), (5, 50)]
+    print(greedy_scaloni_por_ayudante(tiempos))
+    print(greedy_scaloni_por_diferencia(tiempos))
+    print(greedy_scaloni_por_scaloni(tiempos))
 
 
 main()
