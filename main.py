04f262da79f970212b63c98cfd049836d0b57856
matias-gonz <maigonzalez@fi.uba.ar>
2023-09-17 20:22:52 -0300

    Agregar parsear_argumentos

--- main.py
+++ main.py
@@ -1,31 +1,15 @@
-from typing import List, Tuple
+import time
+from typing import List, Tuple, Callable
+import argparse
 
-
-def greedy_scaloni_por_ayudante(tiempos: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
-    """
-    :param tiempos: lista de tuplas con los tiempos de Scaloni y ayudante (S_i, A_i)
-    :return: lista de ordenada por A_i en orden descendiente
-    """
-    return sorted(tiempos, key=lambda t: t[1], reverse=True)
-
-
-def greedy_scaloni_por_diferencia(tiempos: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
-    """
-    :param tiempos: lista de tuplas con los tiempos de Scaloni y ayudante (S_i, A_i)
-    :return: lista de ordenada por A_i - S_i en orden descendiente
-    """
-    return sorted(tiempos, key=lambda t: t[1] - t[0], reverse=True)
-
-
-def greedy_scaloni_por_scaloni(tiempos: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
-    """
-    :param tiempos: lista de tuplas con los tiempos de Scaloni y ayudante (S_i, A_i)
-    :return: lista de ordenada por S_i en orden ascendente
-    """
-    return sorted(tiempos, key=lambda t: t[0])
+from algoritmos import greedy_scaloni_por_ayudante, greedy_scaloni_por_diferencia, greedy_scaloni_por_scaloni
 
 
 def cargar_tiempos(path) -> List[Tuple[int, int]]:
+    """
+    :param path: path del archivo con los tiempos en formato Si,Ai
+    :return: lista de tuplas con los tiempos de Scaloni y ayudante (S_i, A_i)
+    """
     with open(path, "r") as archivo:
         lineas = archivo.readlines()
         tiempos = []
@@ -38,7 +22,7 @@ def cargar_tiempos(path) -> List[Tuple[int, int]]:
     return tiempos
 
 
-def calcular_tiempo_final(tiempos: List[Tuple[int, int]]) -> int:
+def calcular_tiempo_analisis(tiempos: List[Tuple[int, int]]) -> int:
     """
     :param tiempos: lista de tuplas con los tiempos de Scaloni y ayudante (S_i, A_i)
     :return: tiempo final que tomara analizar todos los casos
@@ -51,12 +35,42 @@ def calcular_tiempo_final(tiempos: List[Tuple[int, int]]) -> int:
     return t_final
 
 
+def parsear_argumentos() -> Tuple[str, Callable[[List[Tuple[int, int]]], int]]:
+    """
+    :return: tupla con el path del archivo y la funcion del algoritmo a utilizar
+    """
+    ALGORITMOS = {"ayudante": greedy_scaloni_por_ayudante,
+                  "diferencia": greedy_scaloni_por_diferencia,
+                  "scaloni": greedy_scaloni_por_scaloni}
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--path", help="Ruta al archivo de tiempos", required=True)
+    parser.add_argument("-a", "--algoritmo",
+                        help="Algoritmo a utilizar",
+                        choices=["ayudante", "diferencia", "scaloni"],
+                        default="ayudante")
+    args = parser.parse_args()
+    path = args.path
+    algoritmo = ALGORITMOS[args.algoritmo]
+
+    return path, algoritmo
+
+
 def main():
-    tiempos = cargar_tiempos("casos/10000 elem.csv")
-    print(calcular_tiempo_final(tiempos))
-    print(calcular_tiempo_final(greedy_scaloni_por_ayudante(tiempos)))
-    print(calcular_tiempo_final(greedy_scaloni_por_diferencia(tiempos)))
-    print(calcular_tiempo_final(greedy_scaloni_por_scaloni(tiempos)))
+    path, algoritmo = parsear_argumentos()
+
+    tiempos = cargar_tiempos(path)
+    print("Archivo {} cargado correctamente con n={}".format(path, len(tiempos)))
+
+    print("Utilizando algoritmo {}".format(algoritmo.__name__))
+    t_i = time.time()
+    tiempos_ordenados = algoritmo(tiempos)
+    t_f = time.time()
+    print("Tiempo de ejecucion: {} segundos".format(t_f - t_i))
+
+    tiempo_analisis = calcular_tiempo_analisis(tiempos_ordenados)
+    print("Tiempo final del analisis: {}".format(tiempo_analisis))
 
 
-main()
+if __name__ == "__main__":
+    main()
